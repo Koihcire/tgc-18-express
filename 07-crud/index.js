190ab5f7a57aabbc1f2d9a18dbcecd78208e345b
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-06-06 19:41:40 +0800

    hands on

--- 07-crud/index.js
+++ 07-crud/index.js
@@ -5,7 +5,7 @@ const axios = require("axios"); //for access to resful api
 
 const app = express();
 app.set("view engine", "hbs");
-app.use(express.static("public"));
+// app.use(express.static("public"));
 waXon.on(hbs.handlebars);
 waXon.setLayoutPath("./views/layouts");
 app.use(express.urlencoded({extended:false}));
@@ -23,7 +23,6 @@ app.get("/", async function(req,res){
 app.get("/food_sighting/create", function(req,res){
     res.render("food_form");
 })
-
 app.post("/food_sighting/create", async function(req,res){
     let data = {
         "description" : req.body.description,
@@ -33,6 +32,34 @@ app.post("/food_sighting/create", async function(req,res){
     await axios.post(BASE_API_URL + "sighting", data);
     res.redirect("/");
 })
+
+//edit post
+app.get("/food_sighting/edit/:food_sighting_id", async function(req,res){
+    try{
+    let foodSightingId = req.params.food_sighting_id;
+    let response = await axios.get(BASE_API_URL + "sighting/" + foodSightingId);
+    let foodSighting = response.data;
+    res.render("edit_food_form", {
+        "description" : foodSighting.description,
+        "food" : foodSighting.food,
+        "datetime" : foodSighting.datetime.slice(0, -1)
+    });
+    } catch(e){
+        console.log(e);
+    }
+})
+app.post("/food_sighting/edit/:food_sighting_id", async function(req,res){
+    let data = {
+        "description" : req.body.description,
+        "food" : req.body.food.split(","),
+        "datetime" : req.body.datetime
+    }
+    let foodSightingId = req.params.food_sighting_id;
+    await axios.put(BASE_API_URL + "sighting/" + foodSightingId , data)
+
+    res.redirect("/");
+})
+
 
 //open port
 app.listen(3000,function(){
---
+++ 07-handson/index.js
@@ -0,0 +1,75 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const hbs = require('hbs');
+const waxOn = require('wax-on');
+
+
+let app = express(); //create the express application
+app.set('view engine', 'hbs'); // inform express that we are using hbs as the view engine
+waxOn.on(hbs.handlebars); // enable wax-on for handlebars (for template inheritance)
+waxOn.setLayoutPath('./views/layouts') // inform wax-on where to find the layouts
+app.use(express.static("public"));
+
+app.use(express.urlencoded({
+    'extended':false // for processing HTML forms usually it's false because
+                     // HTML forms are usually quite simple
+}))
+
+// routes
+const BASE_API_URL = "https://ckx-movies-api.herokuapp.com/";
+
+//display movies data
+app.get('/movies', async function(req,res){
+    let response = await axios.get(BASE_API_URL + "movies");
+    res.render("movies" , {
+        "movies" : response.data
+    })
+})
+
+//add a new entry
+app.get("/movie/create", function(req,res){
+    res.render("create");
+})
+app.post("/movie/create", async function(req,res){
+    let data = {
+        "title" : req.body.title,
+        "plot" : req.body.plot
+    }
+    await axios.post(BASE_API_URL + "movie/create" , data);
+    res.redirect("/movies")
+})
+
+//update an existing entry
+app.get("/movie/update/:movie_id", async function(req,res){
+    let response = await axios.get(BASE_API_URL + "movie/" + req.params.movie_id);
+    let movie = response.data;
+    res.render("update_movie", {
+        "movie" : movie
+    })
+})
+app.post("/movie/update/:movie_id", async function(req,res){
+    let movie = {
+        "title" : req.body.title,
+        "plot" : req.body.plot
+    }
+    await axios.patch(BASE_API_URL + "movie/" + req.params.movie_id, movie);
+    res.redirect("/movies");
+})
+
+//delete a movie entry
+app.get("/movie/delete/:movie_id", async function(req,res){
+    let response = await axios.get(BASE_API_URL + "movie/" + req.params.movie_id);
+    let movie = response.data;
+    res.render("delete_movie", {
+        "movie" : movie
+    })
+})
+app.post("/movie/delete/:movie_id", async function(req,res){
+    let response = await axios.delete(BASE_API_URL + "movie/" + req.params.movie_id);
+    res.redirect("/movies");
+})
+
+app.listen(3000, function(){
+    console.log("server started");
+})
+
